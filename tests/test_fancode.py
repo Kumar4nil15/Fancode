66095584fd387687896b663b693aed5448ee1270
Kumar4nil15 <Anilkdyadav15@gmail.com>
2024-07-14 13:29:39 +0530

    Update test_fancode.py

--- tests/test_fancode.py
+++ tests/test_fancode.py
@@ -1,1 +1,7 @@
+import pytest
+from main import check_fancode_users_completion
 
+def test_fancode_users_completion():
+    result = check_fancode_users_completion()
+    for user, is_completed in result.items():
+        assert is_completed, f"{user} has not completed more than 50% of their to-do tasks"
